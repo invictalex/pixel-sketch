d604b8444821325a50f183295abf494a2c03256d
Alex Christodoulou <alexchristodoulou@ymail.com>
2022-06-27 15:11:35 +0100

    Finalise all base functionality

--- README.md
+++ README.md
@@ -16,19 +16,19 @@ In terms of functionality, the tool will have three toggle checkboxes; pencil/er
 
 ----------STAGE 2: FUNCTIONALITY----------
 
-3. Dynamically create a grid of divs. 
+3. Dynamically create a grid of divs. ✔
 
-4. Create a checkbox that changes the grid size on check.
+4. Create a checkbox that changes the grid size on check. ✔
 
-5. Write a "marker" function that: when a div is clicked and dragged through, it turns black. 
+5. Write a "marker" function that: when a div is clicked and dragged through, it turns black. ✔
 
-6. Create a button which resets the grid to white when clicked.
+6. Create a button which resets the grid to white when clicked. ✔
 
-7. Write a function that: allows the user to input a hex value which determines the color used in "marker".
+7. Write a function that: allows the user to input a hex value which determines the color used in "marker". ✔
 
-8. Create a checkbox that, when checked, forces "marker" to output black, irrespective of hex color selection.
+8. Create a checkbox that, when checked, forces "marker" to output black, irrespective of hex color selection. ✔
 
-9. Create a checkbox that changes the color mode of the tool from dark to light on check.
+9. Create a checkbox that changes the color mode of the tool from dark to light on check.✔
 
 ----------STAGE 3: POLISH INTERFACE----------
 
--- script.js
+++ script.js
@@ -3,58 +3,203 @@ var gridStyle = getComputedStyle(grid);
 var gridPx = parseInt(gridStyle.width);
 var slider = document.querySelector("#gridSlider");
 var resetBtn = document.querySelector(".reset");
+var eraser = document.querySelector("#eraserCheckbox");
+var colorModeLight = document.querySelector("#colorModeChbx");
+var darkColors = ["white", "grey", "red", "orange", "yellow", "green", "blue", "purple"];
+var lightColors = ["black", "grey", "rosybrown", "teal", "tan", "goldenrod", "bueviolet", "brown"];
 
+
+//                                      INITIAL SETTINGS
+
+var eraserColor = "black";
+var defaultColor = "white";
+var colorMode = "dark";
 var mouseDown = false;
+var selectedColor;
 
-window.onmousedown = () => 
-{  
-    mouseDown = true;
-}  
 
-window.onmouseup = () => 
-{  
-    mouseDown = false;
-}
+//                                      LIVE FUNCTIONS
 
+trackMousePress();
+
+genGrid(slider.value);
+
+changeColorMode();
 
 resetBtn.onclick = () => genGrid(slider.value);
+
+draw();
+
+colorModeLight.onclick = () => changeColorMode();
+
+
+
+//                                      FUNCTION DEFINITIONS
+
+
+
+function trackMousePress()
+{
+    window.onmousedown = () => 
+    {  
+        mouseDown = true;
+    }  
+    
+    window.onmouseup = () => 
+    {  
+        mouseDown = false;
+    }
+}
 
 function draw() 
 {
     grid.onmousemove = (e) =>
     {
-        if (mouseDown)
+        if (mouseDown && inside)
         {
             var square = e.path[0];
-            square.style.backgroundColor = "white";
+            if (square.classList.contains("square"))
+            {
+                square.style.backgroundColor = markerColor();
+            }
+            
         }
     }
     
     grid.onmousedown = (e) =>
     {
         var square = e.path[0];
-        square.style.backgroundColor = "white";
+        square.style.backgroundColor = markerColor();
+    }
+}
+
+function pickColor(num)
+{
+    if (colorMode == "dark")
+    {
+        
+        return darkColors[num];
+    } 
+    else if (colorMode == "light")
+    {
+        
+        return lightColors[num];
     }
 }
 
 
+function markerColor()
+{
+    if (eraser.checked == true)
+        
+        return eraserColor;
+    
+    else if (eraser.checked == false && selectedColor !== undefined)
 
+        return selectedColor;
 
+    else if (eraser.checked == false && selectedColor === undefined)
 
-
+        return defaultColor;
+}
 
 function genGrid(num)
 {
     grid.textContent = "";
     var totalSquares = num * num;
-    var squarePx = (gridPx / num) - 2;
+    var squarePx = (gridPx / num);
 
     for (let i = 0; i < totalSquares; i++)
     {
         var square = document.createElement("div");
         square.classList.add("square");
         /*square.setAttribute("id", `${i}`);*/
-        square.setAttribute(`style`, `border: solid 1px white; width:${squarePx}px; height:${squarePx}px;`);
+        square.setAttribute(`style`, `width:${squarePx}px; height:${squarePx}px;`);
         grid.appendChild(square);
     }
+}
+
+function changeColorMode()
+{
+    changeArtboardColors();
+    
+    var body = document.querySelector("body");
+    var tools = document.querySelector(".tools");
+    var grid = document.querySelector(".grid");
+    var swatch = document.querySelectorAll(".swatchBox");
+
+    if (colorModeLight.checked == true)
+    {
+        colorMode = "light";
+        defaultColor = "black";
+        eraserColor = "white";
+        
+
+        tools.classList.add("light");
+        grid.classList.add("light");
+        body.classList.add("light");
+
+        swatch.forEach(color  =>
+        {
+            color.classList.add("light");
+        })
+
+        for (let i = 0; i < 7; i++)
+        {
+            if (selectedColor == darkColors[i])
+            {
+                selectedColor = lightColors[i];
+            }
+        }
+    }
+
+    if (colorModeLight.checked == false)
+    {
+
+        colorMode = "dark";
+        defaultColor = "white";
+        eraserColor = "black";
+
+        tools.classList.remove("light");
+        grid.classList.remove("light");
+        body.classList.remove("light");
+
+        swatch.forEach(color  =>
+        {
+            color.classList.remove("light");
+        })
+
+        for (let i = 0; i < 7; i++)
+        {
+            if (selectedColor == lightColors[i])
+            {
+                selectedColor = darkColors[i];
+            }
+        }
+    }
+}
+
+function changeArtboardColors()
+{
+    oldMode = lightColors;
+    newMode = darkColors;
+
+    if (colorModeLight.checked == true)
+    {
+        oldMode = darkColors;
+        newMode = lightColors;
+    }
+    var squares = document.querySelectorAll(".square");
+    console.log(squares);
+
+    squares.forEach(square =>
+        {
+            for (let i = 0; i < 7; i++)
+            {
+                if (square.style.backgroundColor == oldMode[i])
+                {
+                    square.style.backgroundColor = newMode[i];
+                }
+            }
+        })
 }
